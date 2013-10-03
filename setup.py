c52b6e0abb8f5cb944e7a21b83e1f0bc5bc83434
Hugo Branquinho <hugobranq@gmail.com>
2013-10-03 16:21:05 +0100

    added manifest for missing locale and templates folders

--- setup.py
+++ setup.py
@@ -8,22 +8,22 @@ from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 try:
-    README = open(os.path.join(here, 'README')).read()
-    CHANGES = open(os.path.join(here, 'CHANGES')).read()
+    with open(os.path.join(here, 'README')) as f:
+        README = f.read()
+    with open(os.path.join(here, 'CHANGES')) as f:
+        CHANGES = f.read()
 except IOError:
     README = CHANGES = ''
 
 requires = ['setuptools',
             'pyramid >= 1.0',
-            'lingua >= 1.3',
             'Babel >= 0.9.6',
             'WebOb >= 1.2b3',
             'translationstring >= 0.4']
 
 python_version = sys.version_info[:2]
 if python_version <= (2, 6):
-    requires.extend([
-        'importlib'])
+    requires.extend(['importlib'])
 
 tests_require = ['WebTest',
                  'zope.component']
@@ -31,34 +31,45 @@ tests_require = ['WebTest',
 testing_extras = tests_require + ['nose',
                                   'coverage']
 
-setup(name='ines',
-      version=__version__,
-      description='Web applications manager for pyramid packages',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-          'Development Status :: 1 - Planning',
-          'Intended Audience :: Developers',
-          'Natural Language :: English',
-          'Framework :: Pyramid',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: Unix',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'License :: OSI Approved :: MIT License',
-          'Topic :: Internet :: WWW/HTTP',
-          'Topic :: Internet :: WWW/HTTP :: WSGI',
-          'Topic :: Utilities'],
-      keywords='web wsgi pylons pyramid utilities core',
-      author='Hugo Branquinho',
-      author_email='hugobranq@gmail.com',
-      url='https://github.com/hugobranq/ines',
-      download_url='https://github.com/hugobranq/ines/tarball/0.1',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=tests_require,
-      test_suite='ines.tests',
-      extras_require={'testing': testing_extras},
-      message_extractors={'ines': [('**.py', 'lingua_python', None),
-                                   ('**.pt', 'lingua_xml', None)]})
+setupkw = dict(
+    name='ines',
+    version=__version__,
+    description='Web applications manager for pyramid packages',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=['Development Status :: 1 - Planning',
+                 'Intended Audience :: Developers',
+                 'Natural Language :: English',
+                 'Framework :: Pyramid',
+                 'Operating System :: MacOS :: MacOS X',
+                 'Operating System :: Unix',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Internet :: WWW/HTTP',
+                 'Topic :: Internet :: WWW/HTTP :: WSGI',
+                 'Topic :: Utilities'],
+    keywords='web wsgi pylons pyramid utilities core',
+    author='Hugo Branquinho',
+    author_email='hugobranq@gmail.com',
+    url='https://github.com/hugobranq/ines',
+    download_url='https://github.com/hugobranq/ines/tarball/0.1',
+    license='MIT license',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=tests_require,
+    test_suite='ines.tests',
+    extras_require={'testing': testing_extras})
+
+
+try:
+    import babel
+    babel = babel # PyFlakes
+    setupkw['message_extractors'] = {
+        'ines': [('**.py', 'lingua_python', None),
+                 ('**.pt', 'lingua_xml', None)]}
+except ImportError:
+    pass
+
+setup(**setupkw)
