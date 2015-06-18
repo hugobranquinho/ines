e3986c7621493f2844a7588444abfb830f5d5005
Hugo Branquinho <hugobranq@gmail.com>
2015-06-18 21:58:25 +0100

    New method ``api.route_url``. Creates the application route_url like request.route_url

--- ines/api/__init__.py
+++ ines/api/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from pyramid.decorator import reify
+from pyramid.interfaces import IRoutesMapper
+from pyramid.url import _join_elements
+from pyramid.url import parse_url_overrides
 from zope.interface import implementer
 
 from ines.interfaces import IBaseSessionManager
@@ -63,6 +66,40 @@ class BaseSession(object):
     @reify
     def applications(self):
         return self.request.applications
+
+    def route_url(self, route_name, *elements, **kw):
+        """ See: pyramid.URLMethodsMixin.route_url
+        """
+
+        mapper = self.registry.getUtility(IRoutesMapper)
+        route = mapper.get_route(route_name)
+
+        if route is None:
+            raise KeyError('No such route named %s' % route_name)
+
+        if route.pregenerator is not None:
+            elements, kw = route.pregenerator(self, elements, kw)
+
+        app_url, scheme, host, port, qs, anchor = parse_url_overrides(kw)
+
+        if app_url is None:
+            if scheme is not None or host is not None or port is not None:
+                app_url = self._partial_application_url(scheme, host, port)
+            else:
+                app_url = self.request.host_url
+                if self.config.settings.get('app_url'):
+                    app_url = '/'.join(s.strip('/') for s in [app_url, self.config.settings['app_url']])
+
+        path = route.generate(kw) # raises KeyError if generate fails
+
+        if elements:
+            suffix = _join_elements(elements)
+            if not path.endswith('/'):
+                suffix = '/' + suffix
+        else:
+            suffix = ''
+
+        return app_url + path + suffix + qs + anchor
 
 
 class BaseAPISession(BaseSession):
